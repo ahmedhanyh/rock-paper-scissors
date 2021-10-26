1bb337dbf49ca2f99ad5aea87c381cbd375ac981
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-10-26 20:39:28 +0200

    Update the steps in script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-// TODO: Create a string variable computerSelection
-// TODO: Create a string variable playerSelection
 // TODO: Create a function that simulates the computer's turn
-// TODO: Store computerPlay() result in computerSelection
-// TODO: Simulate the player's turn (by receiving user input and storing the value in playerSelection)
+
 // TODO: Create a function to simulate a single game round
+
 // TODO: Create a function to start a game consisting of five rounds
+
+    // TODO: Store computerPlay() result in computerSelection
+
+    // TODO: Simulate the player's turn (by receiving user input and storing the value in playerSelection)
+
+    // TODO: Simulate a game round
+
 // TODO: Start the game
