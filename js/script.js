9a4a1918b752c57ca9cdf5412101f60a99f9dc45
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-10-26 21:26:38 +0200

    Create playRound() function

--- js/script.js
+++ js/script.js
@@ -14,7 +14,32 @@ function computerPlay() {
     }
 }
 
-// TODO: Create a function to simulate a single game round
+// Create a function to simulate a single game round
+function playRound(playerSelection, computerSelection) {
+
+    // Change all playerSelection letters to lower case 
+    playerSelection = playerSelection.toLowerCase();
+
+    // Change all computerSelection letters to lower case
+    computerSelection = computerSelection.toLowerCase();
+
+    // Return the result
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lose! Paper beats Rock';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock beats Scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors beats Paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper beats Rock'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats Scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors beats Paper'
+    }
+}
 
 // TODO: Create a function to start a game consisting of five rounds
 
