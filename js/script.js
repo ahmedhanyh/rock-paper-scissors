0b517f426209ef1d23d12b3a792c8629a3e23735
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-10-26 20:40:29 +0200

    Create computerPlay() function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-// TODO: Create a function that simulates the computer's turn
+// Create a function that simulates the computer's turn
+function computerPlay() {
+    
+    // Generate a random number between 0 and 3 exclusively
+    let selection = Math.floor(Math.random() * 3);
+    
+    // Map each of 0, 1 and 2 to rock, paper and scissors
+    if (selection === 0) {
+        return 'rock';
+    } else if (selection === 1) {
+        return 'paper';
+    } else if (selection === 2) {
+        return 'scissors';
+    }
+}
 
 // TODO: Create a function to simulate a single game round
 
