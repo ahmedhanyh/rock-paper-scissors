fe7df8bb31d4f069f0d6384b3fd0b8a54782e9ce
Ahmed Hany <ahmedhanyhosny@outlook.com>
2021-10-26 20:24:42 +0200

    Putting the steps for the program in script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// TODO: Create a string variable computerSelection
+// TODO: Create a string variable playerSelection
+// TODO: Create a function that simulates the computer's turn
+// TODO: Store computerPlay() result in computerSelection
+// TODO: Simulate the player's turn (by receiving user input and storing the value in playerSelection)
+// TODO: Create a function to simulate a single game round
+// TODO: Create a function to start a game consisting of five rounds
+// TODO: Start the game
